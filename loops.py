3028df9c977c7c2974c723c4d13fba0533bf22c3
manireddy7989 <manikanth.banswada@gmail.com>
2024-03-17 09:48:04 +0530

    learned about while and for loops

---
+++ loops.py
@@ -0,0 +1,12 @@
+count = 1 
+while count <= 5:
+    print(count)
+    count += 1
+    
+my_list1 = [1,2,3]
+my_list2 = [4,5,6]
+ 
+for i in my_list1:
+    for j in my_list2:
+        print(i,j)
+        
